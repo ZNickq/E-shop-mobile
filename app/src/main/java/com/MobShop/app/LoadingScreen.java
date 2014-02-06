eea03e2e22c9740bfdaa4ec483b363fb7463ded0
mihai <cumbaia97@yahoo.co.uk>
2014-02-06 22:31:08 +0200

    Initial commit

---
+++ app/src/main/java/com/MobShop/app/LoadingScreen.java
@@ -0,0 +1,92 @@
+package com.MobShop.app;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.view.Window;
+import android.widget.ProgressBar;
+
+public class LoadingScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_loading_screen);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.loading_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            final View rootView = inflater.inflate(R.layout.fragment_loading_screen, container, false);
+
+
+            Handler handler = new Handler();
+
+            Runnable r=new Runnable()
+            {
+                public void run()
+                {
+                    PlaceholderFragment.this.getActivity().runOnUiThread(new Runnable() {
+
+                        public void run() {
+                            Intent it = new Intent(getActivity(), MainActivity.class);
+                            startActivity(it);
+                        }
+
+                    });
+                }
+            };
+            handler.postDelayed(r, 3000);
+
+
+            return rootView;
+        }
+    }
+
+}
